dc538204c4e15bb31580b9617f09a21f0d28b508
Minqi Lu <minqilu@MinqideMacBook-Pro.local>
2024-09-28 16:25:36 +0800

    feat: add data module

---
+++ src/data/index.ts
@@ -0,0 +1,41 @@
+import invoices from './invoices-attelas.json'
+import clients from './clients-attelas.json'
+
+interface LineItem {
+  description: string
+  quantity: number
+  price: number
+}
+
+interface Invoice {
+  invoiceNumber: string
+  clientId: string
+  dueDate: string
+  status: 'Paid' | 'Pending' | 'Overdue'
+  lineItems: LineItem[]
+}
+
+interface Client {
+  clientId: string
+  name: string
+  email: string
+  address: string
+}
+
+export type InvoiceDetail = Invoice & Client & {
+  totalAmountDue: number
+}
+
+export const getInvoices = (): InvoiceDetail[] => {
+  return (invoices as Invoice[]).map(({ clientId, ...rest }) => {
+    const client = (clients as Client[]).find(client => client.clientId === clientId)
+
+    if (!client) {
+      throw new Error('[getInvoices] Client not found')
+    }
+
+    const totalAmountDue = rest.lineItems.reduce((acc, item) => acc + item.quantity * item.price, 0)
+
+    return { ...rest, ...client, totalAmountDue }
+  })
+}
